4d08b36f394b670082b234866c5db0d487fc5da2
Nicholas Echols <stere.ereo@gmail.com>
2021-02-20 22:15:33 -0800

    add comment to find more ways to use api return object

--- weather.py
+++ weather.py
@@ -10,13 +10,15 @@ t_lat = 39.324314
 t_lon = -120.179020
 
 mgr = owm.weather_manager()
-one_call = mgr.one_call(lat=t_lat, lon=t_lon)
+
+# this gives an array of pyowm.weatherapi25.weather objects https://pyowm.readthedocs.io/en/latest/_modules/pyowm/weatherapi25/weather.html
+forecast = mgr.one_call(lat=t_lat, lon=t_lon).forecast_daily
 
 
 def get_temp(weather):
     return weather.temperature('fahrenheit')
 
 
-temps = map(get_temp, one_call.forecast_daily)
+temps = map(get_temp, forecast)
 
 print(list(temps))
