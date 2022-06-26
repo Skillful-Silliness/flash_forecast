78c38247024a940138f0dbcc0579ccf5f9ea5f53
krismajean <brian.broompeltz@gmail.com>
2022-06-25 20:03:32 -0700

    fix: fix url for update_frontend scriptwq

--- update_frontend.sh
+++ update_frontend.sh
@@ -2,7 +2,7 @@
 
 archive_url=$(curl \
   -H "Accept: application/vnd.github.v3+json" \
-  https://api.github.com/repos/steryereo/weatherlights_frontend/releases/latest 2>&1 | \
+  https://api.github.com/repos/Skillful-Silliness/flash_forecast_frontend/releases/latest 2>&1 | \
   grep -o '"browser_download_url": "[^"]*' | grep -o '[^"]*$')
 
 curl -L $archive_url -o frontend.zip
