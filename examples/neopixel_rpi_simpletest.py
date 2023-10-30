11fd804b1a152e993384a13703afa88e3c168cab
Brian Broom-Peltz <krismajean@users.noreply.github.com>
2023-10-29 20:26:43 -0700

    Move files to examples folder

--- examples/neopixel_rpi_simpletest.py
+++ examples/neopixel_rpi_simpletest.py
@@ -6,7 +6,6 @@ import time
 import board
 import neopixel
 
-
 # Choose an open pin connected to the Data In of the NeoPixel strip, i.e. board.D18
 # NeoPixels must be connected to D10, D12, D18 or D21 to work.
 pixel_pin = board.D18
@@ -22,7 +21,6 @@ ORDER = neopixel.GRB
 pixels = neopixel.NeoPixel(
     pixel_pin, num_pixels, brightness=0.9, auto_write=False, pixel_order=ORDER
 )
-
 
 def wheel(pos):
     # Input a value 0 to 255 to get a color value.
